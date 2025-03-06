00a5d68982c1cebc0c7d5bed51fde229f088ebd7
Scarounine <paulinelesueur93@gmail.com>
2025-03-06 20:00:31 +0100

    style: spin the refresh button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,18 @@ import { ResaService } from '../services/resa.service';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent {
+  isLoading = false;
+  
   constructor(private resaService: ResaService) {}
 
   refresh(): void {
+    if(this.isLoading) return;
+
+    this.isLoading =true;
     this.resaService.refreshResaList();
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 500);
   }
 }
