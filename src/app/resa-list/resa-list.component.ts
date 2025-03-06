1215a7c54fc3263fea93ebba9aeb139b3707b093
Scarounine <paulinelesueur93@gmail.com>
2025-03-06 17:33:55 +0100

    call http: display content dynamically from json file

--- src/app/resa-list/resa-list.component.ts
+++ src/app/resa-list/resa-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Resa } from '../models/resa';
+import { ResaService } from '../services/resa.service';
 
 @Component({
   selector: 'app-resa-list',
@@ -9,24 +10,15 @@ import { Resa } from '../models/resa';
 export class ResaListComponent implements OnInit {
   resaList: Resa[] = [];
 
+  constructor(private resaService: ResaService) {}
+
   ngOnInit(): void {
     this.loadResa();
   }
 
   loadResa(): void {
-    this.resaList = [
-      {
-        "title": "Réservation 0",
-        "date_1": "2027-01-18",
-        "date_2": "2027-01-22",
-        "description": "Description 0 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit."
-        },
-        {
-        "title": "Réservation 1",
-        "date_1": "2026-09-14",
-        "date_2": "2026-09-19",
-        "description": "Description 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit."
-        }
-    ];
+    this.resaService.findAll().subscribe(data => {
+      this.resaList = data;
+    })
   }
 }
--- src/app/services/resa.service.ts
+++ src/app/services/resa.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResaService {
+  private jsonFile = './assets/api/resa.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {  }
+
+  public findAll(): Observable<any> {
+    return this.http.get<any>(this.jsonFile)
+  }
 }
