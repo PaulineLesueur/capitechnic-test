4592579c61f34acf9e6c586dd6c50a3f9e360bcd
Scarounine <paulinelesueur93@gmail.com>
2025-03-06 16:18:33 +0100

    Functionnal: display dynamically cards from static resource

--- src/app/models/resa.ts
+++ src/app/models/resa.ts
@@ -1,2 +1,13 @@
 export class Resa {
+    title: string;
+    date_1: string;
+    date_2: string;
+    description: string;
+
+    constructor(title: string, date_1: string, date_2: string, description: string) {
+        this.title = title;
+        this.date_1 = date_1;
+        this.date_2 = date_2;
+        this.description = description;
+    }
 }
--- src/app/resa-list/resa-list.component.ts
+++ src/app/resa-list/resa-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Resa } from '../models/resa';
 
 @Component({
   selector: 'app-resa-list',
   templateUrl: './resa-list.component.html',
   styleUrls: ['./resa-list.component.sass']
 })
-export class ResaListComponent {
+export class ResaListComponent implements OnInit {
+  resaList: Resa[] = [];
 
+  ngOnInit(): void {
+    this.loadResa();
+  }
+
+  loadResa(): void {
+    this.resaList = [
+      {
+        "title": "Réservation 0",
+        "date_1": "2027-01-18",
+        "date_2": "2027-01-22",
+        "description": "Description 0 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit."
+        },
+        {
+        "title": "Réservation 1",
+        "date_1": "2026-09-14",
+        "date_2": "2026-09-19",
+        "description": "Description 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit."
+        }
+    ];
+  }
 }
